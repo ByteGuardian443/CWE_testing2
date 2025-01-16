996b5e79192a3ced049ae76f8ec466fe89a29fc9
Sunita L <slan786@outlook.com>
2025-01-15 21:49:31 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+int getValueFromArray(int *array, int len, int index) {
+// Return the element from the array at the specified index
+}
+def get_value_from_array(array, index):
+# Return the element from the array at the specified index
